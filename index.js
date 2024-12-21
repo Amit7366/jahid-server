49b8f50591fecdceaca493d8e3910031db484118
Amit7366 <omit9090@gmail.com>
2024-12-22 00:44:59 +0600

    cors sites 2

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173','https://texts-now.web.app','https://jahid-server.onrender.com','http://localhost:3000'],
+        origin: ['http://localhost:5173','https://texts-now.web.app','https://txtserver.web.app','http://localhost:3000'],
         methods: ['GET', 'POST'],
         
     }
